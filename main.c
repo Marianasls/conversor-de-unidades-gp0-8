f5c28fabc7f778045aa07567c6d94be05c05230e
machadof90 <valterig@hotmail.com>
2024-12-20 08:26:18 -0300

    adicionando funcão conversão de massa

--- main.c
+++ main.c
@@ -88,4 +88,46 @@ void conversao_comprimento(void){
             }
       }
 }
+void conversao_massa(void){
+      printf("CONVERSAO DE MASSA\n\n");
+      printf("[1] QUILOGRAMA --> GRAMA\n[2] QUILOGRAMA --> TONELADA\n");
+      printf("[3] GRAMA --> QUILOGRAMA\n[4] GRAMA --> TONELADA\n");
+      printf("[5] TONELADA --> QUILOGRAMA\n[6] TONELADA --> GRAMA\n");
+      scanf("%d", &escolha);
+      while(escolha < 1 || escolha > 6){
+            printf("Opcao invalida, digite novamente:\n");
+            scanf("%d", &escolha);
+      }
+      if (escolha < 2){
+            printf("Digite o valor em quilogramas:\n");
+            scanf("%f", &valor);
+            if(escolha == 1){
+                  resultado = valor * 1000;
+                  printf("O valor convertido é: %f\n", resultado);
+            } else if(escolha == 2){
+                  resultado = valor / 1000;
+                  printf("O valor convertido é: %f\n", resultado);
+            }
+      } else if(escolha < 4){
+            printf("Digite o valor em gramas:\n");
+            scanf("%f", &valor);
+            if(escolha == 3){
+                  resultado = valor / 1000;
+                  printf("O valor convertido é: %f\n", resultado);
+            } else if(escolha == 4){
+                  resultado = valor / 1000000;
+                  printf("O valor convertido é: %f\n", resultado);
+            }
+      } else {
+            printf("Digite o valor em toneladas:\n");
+            scanf("%f", &valor);
+            if(escolha == 5){
+                  resultado = valor * 1000;
+                  printf("O valor convertido é: %f\n", resultado);
+            } else if(escolha == 6){
+                  resultado = valor * 1000000;
+                  printf("O valor convertido é: %f\n", resultado);
+            }
+      }
+}
 
